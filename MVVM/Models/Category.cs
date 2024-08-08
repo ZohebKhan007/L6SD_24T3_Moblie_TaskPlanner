fc67c0f8b779841cfe77ec077b311f99183bba22
ZohebKhan007 <84551540+ZohebKhan007@users.noreply.github.com>
2024-08-09 09:31:03 +1200

    Update Category.cs
    adding Primary Key which will be used later on in the project

--- MVVM/Models/Category.cs
+++ MVVM/Models/Category.cs
@@ -1,20 +1,20 @@
-﻿using PropertyChanged;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PropertyChanged;
+using SQLite;
 
 namespace TaskPlanner.MVVM.Models
 {
     [AddINotifyPropertyChangedInterface]
     public class Category
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string CategoryName { get; set; } = string.Empty;
         public string Color { get; set; } = string.Empty;
+        [Ignore]
         public int PendingTasks { get; set; }
+        [Ignore]
         public float Percentage { get; set; }
+        [Ignore]
         public bool IsSelected { get; set; }
     }
 }
